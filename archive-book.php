1d19b03bafa86d6f85de63bb5db9f114cfeb69c8
Sharon Zhang <zhangshiyu1992@hotmail.com>
2018-01-16 17:25:10 +0800

    Update archive-book.php

--- archive-book.php
+++ archive-book.php
@@ -4,7 +4,7 @@
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<a href="<?php the_permalink(); ?>">
 		<?php $full_image_url = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full');?>
-			<div class="book" style="background-image:url(<?php //echo $full_image_url[0];?>https://img.js.cn/images/2017/02/06/908fa0ec08fa513dfd470f4b3d6d55fbb3fbd9b2.jpg">
+			<div class="book" style="background-image:url(<?php echo $full_image_url[0];?>">
 				<div class="book-header">
 					<h1><?php the_title(); ?></h1>
 					<?php the_content(); ?>
